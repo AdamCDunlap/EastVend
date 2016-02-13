f2547c309ac948e3d7919598d75dcbb66975673c
Alex Ozdemir <aozdemir@hmc.edu>
2016-02-13 12:11:51 -0800

    Fiddling around with i2c

--- basic.py
+++ basic.py
@@ -1,3 +1,5 @@
+import sys
+
 import itertools
 import RPi.GPIO as GPIO
 import smbus
@@ -5,10 +7,10 @@ import smbus
 import time
 
 motor_arduino_addr = 0x1c
-can_sensor_arduino_addr = 0x1d
+can_sensor_arduino_addr = 0x19
 
 # TODO
-coin_pin = 4
+coin_pin = 25
 #pins that listen to input buttons
 selection_pins = [4, 17, 27, 22, 10, 9, 11]
 #chute correspondence: (1,2), 3, 4, 5, 6, 7 8
@@ -17,16 +19,9 @@ selection_pins = [4, 17, 27, 22, 10, 9, 11]
 bus = smbus.SMBus(1)
 
 def setup_raspi_gpio():
-    for pin in itertools.chain(selection_pins, coin_pin):
+    GPIO.setmode(GPIO.BCM)
+    for pin in itertools.chain(selection_pins, [coin_pin]):
         GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-def write_pin(pin, state):
-    if pin[0] == 0:
-        GPIO.output(pin_to_raw_pin[pin], state)
-    else:
-        addr = arduino_1_addr if pin[0] == 1 else arduino_2_addr
-        code set_pin_code(pin[1], state)
-        bus.write_byte(addr, code)
 
 def valid_swipe_occured():
     # TODO
@@ -35,10 +30,11 @@ def valid_swipe_occured():
 # updates array of fullness
 # when we read a byte, we get a binary number
 # in which each bit is the fullnesss of a chute
-def update_chute_fullness(chute_fullness):
-    byte = bus.read_byte(can_sensor_arduino_addr)
-    bits = [int(x) for x in bin(byte)]
-    chute_fullness[:] = bits(byte)
+def get_chute_fullness():
+    #byte = bus.read_byte(can_sensor_arduino_addr)
+    byte = 0xff
+    bits = [int(x) for x in bin(byte)[2:]]
+    return bits
 
 def get_selection():
     for i, pin in enumerate(selection_pins):
@@ -63,17 +59,19 @@ def main():
     selection = 0
 
     while True:
-        update_chute_fullness(chute_fullness)
+        chute_fullness[:] = get_chute_fullness()
         if state == wait_for_money:
             if not GPIO.input(coin_pin) or valid_swipe_occured():
+                print 'Got money'
                 state = wait_for_selection
         elif state == wait_for_selection:
             selection = get_selection()
             if selection == 1: #user chose random soda
-                selection = random.choice(0,1)
+                selection = random.choice([0,0,0,1,1,1,1,2,3,4,5,6,7])
             if selection > 0 and chute_fullness[selection]:
+                print 'Dispensing', selection
                 bus.write_byte(motor_arduino_addr, selection)
-                state = wait_for_motor_switch_press
+                state = wait_for_money
 
 def test():
     p = can_sensor_pins[3]
@@ -90,4 +88,35 @@ def writeloop():
         state = 0 != input('Value (0 or 1) to write? ')
         write_pin((1, i), state)
 
-test()
+#test()
+
+def testMtr():
+    for i in [4, 5, 6, 7, 2, 3]:
+        print 'Trying to dispense soda %d' % i
+        bus.write_byte(motor_arduino_addr, i)
+        time.sleep(5)
+# for i in xrange(int(sys.argv[2])):
+#     bus.write_byte(motor_arduino_addr, int(sys.argv[1]))
+#     time.sleep(5)
+# 
+print bus.write_byte(motor_arduino_addr, int(sys.argv[1]))
+
+#bus.write_byte(motor_arduino_addr, 0)
+
+#setup_raspi_gpio()
+
+#bus.write_byte(motor_arduino_addr, 1)
+#time.sleep(.1)
+#while True:
+#    print bus.read_byte(can_sensor_arduino_addr)
+#    time.sleep(.1)
+#    #print bus.read_byte(motor_arduino_addr)
+#    #time.sleep(.1)
+# main()
+#while True:
+#    #time.sleep(.2)
+#    try:
+#        print '\n', bus.write_byte(motor_arduino_addr, 3)#can_sensor_arduino_addr)
+#        break
+#    except:
+#        print '.',
